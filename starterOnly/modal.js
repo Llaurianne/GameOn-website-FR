29ace8bc0906a00dee0a288f25e0ad795e9ebfcd
Laurianne <valette.laurianne@gmail.com>
2022-08-18 11:59:05 +0200

    Confirmation message and refactoring

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,18 +9,65 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalbg2 = document.querySelector(".bground2");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelector(".close");
-const first = document.getElementById("first")
-const last = document.getElementById("last")
-const email = document.getElementById("email")
+const closeBtn = document.querySelectorAll(".close");
 const birthDate = document.getElementById("birthdate")
-const quantity = document.getElementById("quantity")
 const locations = document.getElementsByName("location")
 const checkbox1 = document.getElementById("checkbox1")
 const formData = document.querySelectorAll(".formData");
-const form= document.getElementsByName("reserve")
+const form = document.getElementsByName("reserve")
 const inputs = document.getElementsByTagName("input")
+
+let validationArray = []
+
+// storing form validation data
+function updateValidationArray() {
+    // array of arrays [input id or name, validation rule, error message]
+     validationArray = [
+         [
+             'first',
+             /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[0].value),
+             'Prénom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
+         ],
+         [
+             'last',
+             /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[1].value),
+             'Nom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
+         ],
+         [
+             'email',
+             /^([\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]([.]?[\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]+)+)@[\dA-Za-z]([\-]?[\dA-Za-z]+)+.[A-Za-z]*$/.test(inputs[2].value),
+             'Email incorrect ou manquant.'
+         ],
+         [
+             'birthdate',
+             Date.parse(birthDate.value) < Date.now(),
+             /* alternative date validation with regex :
+                regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/
+                return (regex.test(input.value)) */
+             'Date de naissance incorrecte ou manquante.'
+
+         ],
+         [
+             'quantity',
+             /^(0|([1-9]\d?))$/.test(inputs[4].value),
+             /* alternative quantity validation without regex :
+                (Number.isInteger(parseFloat(quantity.value))) && 0<=parseFloat(quantity.value) && parseFloat(quantity.value)<=99) */
+             'Quantité incorrecte ou manquante.'
+         ],
+         [
+             'location',
+             isLocationChecked(),
+             'Veuillez choisir une localisation.'
+         ],
+         [
+             'checkbox1',
+             checkbox1.checked,
+             'Veuillez accepter les conditions d\'utilisation.'
+         ]
+    ]
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,12 +77,27 @@ function launchModal() {
     modalbg.style.display = "block";
 }
 
+// launch modal confirmation
+function launchModal2() {
+    modalbg2.style.display = "block";
+}
+
 // close modal event
-closeBtn.addEventListener("click", closeModal);
+closeBtn.forEach(btn => btn.addEventListener("click", closeModal))
 
 // close modal form
 function closeModal() {
     modalbg.style.display = "none";
+    modalbg2.style.display = "none";
+}
+
+// check if at least one location is checked
+function isLocationChecked() {
+    let locChecked = []
+    for (let loc of locations) {
+        locChecked.push(loc.checked)
+    }
+    return !!locChecked.includes(true);
 }
 
 // hide error messages
@@ -45,98 +107,30 @@ function hideMessages () {
     })
 }
 
-
 // form validation
 function validate() {
+    updateValidationArray()
     hideMessages()
-    let isFormValidated
-    for (let i = 0; i < 12; i++) {
-        if (!isValidated(inputs[i])) {
-            errorMsg(inputs[i]);
-            isFormValidated = false;
+    for (let i = 0; i < 7; i++) {
+        if (!validationArray[i][1]) {
+            errorMsg(validationArray[i][0])
         }
     }
-    if (isFormValidated) {
-        //open next modal;
+    if (validationArray.every(inputArray => inputArray[1] === true)) {
+        closeModal()
+        launchModal2()
+        form[0].reset()
     }
     return false;
 }
 
-function isValidated(input) {
-    let regex
-    switch (input.id) {
-        case 'first' :
-        case 'last' :
-            regex = /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/;
-            return (regex.test(input.value));
-            break
-        case 'email' :
-            regex = /^([\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]([.]?[\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]+)+)@[\dA-Za-z]([\-]?[\dA-Za-z]+)+.[A-Za-z]*$/
-            return (regex.test(input.value))
-            break
-        case 'birthdate' :
-            return (Date.parse(birthDate.value) < Date.now())
-            /* alternative date validation with regex :
-            regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/
-            return (regex.test(input.value)) */
-            break
-        case 'quantity' :
-            regex = /^(0|([1-9]\d?))$/
-            return (regex.test(input.value))
-            /* alternative quantity validation without regex :
-            (Number.isInteger(parseFloat(quantity.value))) && 0<=parseFloat(quantity.value) && parseFloat(quantity.value)<=99) */
-            break
-        case 'location1' :
-        case 'location2' :
-        case 'location3' :
-        case 'location4' :
-        case 'location5' :
-        case 'location6' :
-            let isLocationChecked = false
-            // check if at least one location is checked
-            for (let loc of locations) {
-                if (loc.checked === true) {
-                    isLocationChecked = true
-                }
-            }
-            return isLocationChecked
-            break
-        case 'checkbox1' :
-            return checkbox1.checked;
-            break
-    }
-}
-
 function errorMsg(input) {
-    let errorMsg
-    switch (input.id) {
-        case 'first' :
-            errorMsg = "Prénom incorrect ou manquant. Veuillez entrer au moins 2 caractères.";
-            break
-        case 'last' :
-            errorMsg = "Nom incorrect ou manquant. Veuillez entrer au moins 2 caractères.";
-            break
-        case 'email' :
-            errorMsg = "Email incorrect ou manquant.";
-            break
-        case 'birthdate' :
-            errorMsg = "Date de naissance incorrecte ou manquante.";
-            break
-        case 'quantity' :
-            errorMsg = "Quantité incorrecte ou manquante.";
-            break
-        case 'location1' :
-        case 'location2' :
-        case 'location3' :
-        case 'location4' :
-        case 'location5' :
-        case 'location6' :
-            errorMsg = "Veuillez choisir une localisation.";
-            break
-        case 'checkbox1' :
-            errorMsg = "Veuillez accepter les conditions d\'utilisation.";
-            break
+    let errorMsg = validationArray.find( e => e[0] === input)[2]
+    if (input === 'location') {
+        document.getElementsByName(input)[0].parentElement.setAttribute('data-error', errorMsg)
+        document.getElementsByName(input)[0].parentElement.setAttribute("data-error-visible", "true")
+    } else {
+        document.getElementById(input).parentElement.setAttribute('data-error', errorMsg)
+        document.getElementById(input).parentElement.setAttribute("data-error-visible", "true")
     }
-    input.parentElement.setAttribute('data-error', errorMsg)
-    input.parentElement.setAttribute("data-error-visible", "true")
 }
