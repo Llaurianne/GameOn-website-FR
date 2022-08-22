75ab04da49115360334f1f9d929a4b9048f87009
Laurianne <valette.laurianne@gmail.com>
2022-08-22 11:02:08 +0200

    Replace validation data array with object & add close button to the confirmation message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalbg2 = document.querySelector(".bground2");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelectorAll(".close");
+const closeBtn = document.querySelectorAll(".close,.close-btn");
 const birthDate = document.getElementById("birthdate")
 const locations = document.getElementsByName("location")
 const checkbox1 = document.getElementById("checkbox1")
@@ -19,54 +19,47 @@ const formData = document.querySelectorAll(".formData");
 const form = document.getElementsByName("reserve")
 const inputs = document.getElementsByTagName("input")
 
-let validationArray = []
-
+// variables declarations
+let validationDatas = {}
+console.log(validationDatas)
 // storing form validation data
-function updateValidationArray() {
+function updateValidationDatas() {
     // array of arrays [input id or name, validation rule, error message]
-     validationArray = [
-         [
-             'first',
-             /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[0].value),
-             'Prénom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
-         ],
-         [
-             'last',
-             /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[1].value),
-             'Nom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
-         ],
-         [
-             'email',
-             /^([\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]([.]?[\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]+)+)@[\dA-Za-z]([\-]?[\dA-Za-z]+)+.[A-Za-z]*$/.test(inputs[2].value),
-             'Email incorrect ou manquant.'
-         ],
-         [
-             'birthdate',
-             Date.parse(birthDate.value) < Date.now(),
+     validationDatas = {
+         first : {
+             validationTest : /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[0].value),
+             error : 'Prénom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
+         },
+         last : {
+             validationTest : /^[A-Za-zŠŒŽœžÀ-ÿ\- ']{2,}$/.test(inputs[1].value),
+             error : 'Nom incorrect ou manquant. Veuillez entrer au moins 2 caractères.'
+         },
+         email : {
+             validationTest : /^([\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]([.]?[\dA-Za-z!#$%&'*\/=?^_+\-`{|}~]+)+)@[\dA-Za-z]([\-]?[\dA-Za-z]+)+\.[A-Za-z]{2,}$/.test(inputs[2].value),
+             error : 'Email incorrect ou manquant.'
+         },
+         birthdate : {
+             validationTest : Date.parse(birthDate.value) < Date.now(),
              /* alternative date validation with regex :
-                regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/
-                return (regex.test(input.value)) */
-             'Date de naissance incorrecte ou manquante.'
-
-         ],
-         [
-             'quantity',
-             /^(0|([1-9]\d?))$/.test(inputs[4].value),
+             regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/
+             return (regex.test(input.value)) */
+             error : 'Date de naissance incorrecte ou manquante.'
+         },
+         quantity : {
+             validationTest : /^(0|([1-9]\d?))$/.test(inputs[4].value),
              /* alternative quantity validation without regex :
-                (Number.isInteger(parseFloat(quantity.value))) && 0<=parseFloat(quantity.value) && parseFloat(quantity.value)<=99) */
-             'Quantité incorrecte ou manquante.'
-         ],
-         [
-             'location',
-             isLocationChecked(),
-             'Veuillez choisir une localisation.'
-         ],
-         [
-             'checkbox1',
-             checkbox1.checked,
-             'Veuillez accepter les conditions d\'utilisation.'
-         ]
-    ]
+             (Number.isInteger(parseFloat(quantity.value))) && 0<=parseFloat(quantity.value) && parseFloat(quantity.value)<=99) */
+             error : 'Quantité incorrecte ou manquante.'
+         },
+         location : {
+             validationTest : isLocationChecked(),
+             error : 'Veuillez choisir une localisation.'
+         },
+         checkbox1 : {
+             validationTest : checkbox1.checked,
+             error : 'Veuillez accepter les conditions d\'utilisation.'
+         }
+    }
 }
 
 // launch modal event
@@ -85,7 +78,7 @@ function launchModal2() {
 // close modal event
 closeBtn.forEach(btn => btn.addEventListener("click", closeModal))
 
-// close modal form
+// close modal form & confirmation message
 function closeModal() {
     modalbg.style.display = "none";
     modalbg2.style.display = "none";
@@ -109,23 +102,29 @@ function hideMessages () {
 
 // form validation
 function validate() {
-    updateValidationArray()
+    updateValidationDatas()
+    console.log(validationDatas)
     hideMessages()
-    for (let i = 0; i < 7; i++) {
-        if (!validationArray[i][1]) {
-            errorMsg(validationArray[i][0])
+    // browses the array of inputs and display error messages if invalid
+    for (let input in validationDatas) {
+        console.log(validationDatas[input].validationTest)
+        if (!validationDatas[input].validationTest) {
+            errorMsg(input)
         }
     }
-    if (validationArray.every(inputArray => inputArray[1] === true)) {
+    // close modal if everything's right
+    if (Object.values(validationDatas).every(data => data.validationTest === true)) {
         closeModal()
         launchModal2()
         form[0].reset()
     }
+    // overrides the default form submission behavior
     return false;
 }
 
+// add specific error message as attribute to the input's parent if invalid
 function errorMsg(input) {
-    let errorMsg = validationArray.find( e => e[0] === input)[2]
+    let errorMsg = validationDatas[input].error
     if (input === 'location') {
         document.getElementsByName(input)[0].parentElement.setAttribute('data-error', errorMsg)
         document.getElementsByName(input)[0].parentElement.setAttribute("data-error-visible", "true")
